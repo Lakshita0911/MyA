fed0c21fb850afb38b0b534b0d24400f9a934016
Lakshita0911 <lakshitarathod111@gmail.com>
2025-01-08 07:44:45 -0500

    Update dldpr.c

--- dldpr.c
+++ dldpr.c
@@ -1,20 +1,20 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 struct data {
     long long aadhar_no;
-    char name[50], add[100], lm[50], dis[50], st[50], nationality[20], dob[20];
+    char name[50], add[100], lm[50], dis[50], st[50], nationality[20], dob[20],cd[20],ud[20];
     char f_name[50];
     int age, pin;
-    long phn;
+    long long int phn;
     char gender;
 } d;
 
+
 void registration();
 void login();
+void operatormenu();
 void update();
 void display();
 void remove_record();
@@ -22,58 +22,111 @@ void count_records();
 void list_all_users();
 void search_record();
 int validate_aadhar(long long aadhar_no);
-long long generate_aadhar();
-
-int main() {
-    int ch;
-
-    while (1) {
-        printf("\n\t\tWELCOME TO MY AADHAR SERVICE\n");
-        printf("1. REGISTRATION\n2. LOGIN\n3. UPDATE\n4. DISPLAY\n5. REMOVE\n6. SEARCH\n7. LIST ALL USERS\n8. COUNT RECORDS\n9. Exit\n");
+void print_card();
+int main(){
+	int c,usertype;
+	main:
+	while(1){
+		printf("\n-----ADHAR CARD MANAGEMENT SYSTEM------\n");
+		printf("1.operator view\n2.user view\n3.exit\n");
+		printf("select user type:\n");
+		scanf("%d",&usertype);
+        
+        switch(usertype){
+        	case 1:login();
+        	break;
+        	case 2:
+        		while(1){
+				printf("\n-----user menu-----\n");
+				printf("1.search record\n2.view details\n3.back to menu\n");
+				printf("select  your choice:");
+				scanf("%d",&c);
+				 switch(c){
+				 	case 1: search_record();
+				 	break;
+				 	case 2: print_card();
+				 	break;
+				 	case 3:goto main;
+				 	default:
+				 		printf("invalid input! try again\n");
+				 }
+				}
+				case 3: printf("exiting\n");
+				       exit(0);
+				default:printf("invalid input! try again\n");       
+					}
+	}
+	
+	mainmenu:
+		return 0;
+}
+ 
+ void login(){
+ 	char username[50],password[50];
+ 	printf("\nenter operator username: ");
+ 	scanf("%s",username);
+ 	printf("\nenter operator password:");
+ 	scanf("%s",password);
+ 	
+ 	if((strcmp(username,"lakshita")==0 && (strcmp(password,"november9")==0))||(strcmp(username,"deepshikha")==0 && (strcmp(password,"june3")==0))||(strcmp(username,"divya")==0 && (strcmp(password,"jan6")==0)))
+ 	   {
+ 	   	 printf("login successfully\n");
+ 	   	 operatormenu();
+		}
+		else{
+			printf("invalid !try again\n");
+		}
+ }
+ 
+ void operatormenu(){
+ 	int c;
+	 char n;
+ 	
+ 	while(1){
+ 		printf("\n-----operator menu------\n");
+ 		printf("1. REGISTRATION\n2. UPDATE\n3. DISPLAY\n4. REMOVE\n5. SEARCH\n6. LIST ALL USERS\n7. COUNT RECORDS\n8. log out\n");
         printf("Enter your choice: ");
-        scanf("%d", &ch);
+        scanf("%d", &c);
         getchar();
 
-        switch (ch) {
-            case 1:
-                registration();
-                break;
+        switch (c) {
+            case 1: 
+                    registration();
+                    break;
+           
             case 2:
-                login();
+               update();
                 break;
             case 3:
-                update();
-                break;
-            case 4:
                 display();
                 break;
+            case 4:
+               remove_record();
+                break;
             case 5:
-                remove_record();
+               search_record();
                 break;
             case 6:
-                search_record();
-                break;
-            case 7:
                 list_all_users();
                 break;
-            case 8:
+            case 7:
                 count_records();
                 break;
+            case 8:
+                printf("logging out..\n");
+                return;
             case 9:
-                exit(0);
-                break;
+                  print_card();
+                 return;
+                
             default:
                 printf("Invalid choice\n");
                 break;
-        }
-    }
+	 }
+ }
 }
 
-long long generate_aadhar() {
-    srand(time(0));
-    long long aadhar = 100000000000LL + rand() % 900000000000LL;
-    return aadhar;
-}
+
 
 int validate_aadhar(long long aadhar_no) {
     int digits = 0;
@@ -85,32 +138,29 @@ int validate_aadhar(long long aadhar_no) {
 }
 
 void registration() {
-    FILE *fp = fopen("user.data", "ab+");
+
+	
+    FILE *fp = fopen("data", "ab+");
     if (fp == NULL) {
         printf("Error: Cannot open the file\n");
         return;
     }
-
+    
     printf("Enter the user name: ");
     fgets(d.name, sizeof(d.name), stdin);
 
-
-    printf("Enter the father's name: ");
+    printf("Enter the father's name/husband name: ");
     fgets(d.f_name, sizeof(d.f_name), stdin);
-    
 
     printf("Enter your Address: ");
     fgets(d.add, sizeof(d.add), stdin);
     
-
     printf("Enter any landmark: ");
     fgets(d.lm, sizeof(d.lm), stdin);
-    
 
     printf("Enter district: ");
     fgets(d.dis, sizeof(d.dis), stdin);
     
-
     printf("Enter the state: ");
     fgets(d.st, sizeof(d.st), stdin);
     
@@ -126,11 +176,12 @@ void registration() {
     printf("Enter your date of birth (dd/mm/yyyy): ");
     fgets(d.dob, sizeof(d.dob), stdin);
     
-
-    printf("Enter your nationality: ");
-    fgets(d.nationality, sizeof(d.nationality), stdin);
+     printf("Enter current date(dd/mm/yyyy): ");
+    fgets(d.cd, sizeof(d.cd), stdin);
     
-
+	printf("Enter your nationality: ");
+    fgets(d.nationality, sizeof(d.nationality), stdin);
+ 	
     printf("Enter your age: ");
     scanf("%d", &d.age);
     getchar();
@@ -138,8 +189,8 @@ void registration() {
     printf("Enter your gender (M/F): ");
     scanf(" %c", &d.gender);
 
-    d.aadhar_no = generate_aadhar();
-    printf("Aadhar no generated: %lld\n", d.aadhar_no);
+   printf("enter adhar number:");
+   scanf("%lld",&d.aadhar_no);
 
     if (fwrite(&d, sizeof(d), 1, fp) == 1) {
         printf("\nRegistration successful!\n");
@@ -150,41 +201,10 @@ void registration() {
     fclose(fp);
 }
 
-void login() {
-    FILE *fp = fopen("user.data", "rb");
-    if (fp == NULL) {
-        printf("Error: Cannot open the file\n");
-        return;
-    }
 
-    char input_username[50];
-    long input_phn;
-    int found = 0;
-
-    printf("Enter user name: ");
-    fgets(input_username, sizeof(input_username), stdin);
-    
-
-    printf("Enter phone no: ");
-    scanf("%ld", &input_phn);
-
-    while (fread(&d, sizeof(d), 1, fp)) {
-        if (strcmp(input_username, d.name) == 0 && input_phn == d.phn) {
-            printf("Login successful\n");
-            found = 1;
-            break;
-        }
-    }
-
-    if (!found) {
-        printf("User not found or incorrect details\n");
-    }
-
-    fclose(fp);
-}
 
 void update() {
-    FILE *fp = fopen("user.data", "rb+");
+    FILE *fp = fopen("data", "rb+");
     if (fp == NULL) {
         printf("Error: Cannot open the file\n");
         return;
@@ -212,7 +232,7 @@ void update() {
             fgets(d.name, sizeof(d.name), stdin);
            
 
-            printf("Enter new father's name: ");
+            printf("Enter new father's/husband name: ");
             fgets(d.f_name, sizeof(d.f_name), stdin);
            
 
@@ -242,6 +262,10 @@ void update() {
             getchar();
             fgets(d.dob, sizeof(d.dob), stdin);
             
+             printf("Enter new current date(dd/mm/yyyy): ");
+            fgets(d.ud, sizeof(d.ud), stdin);
+    
+            
 
             printf("Enter new age: ");
             scanf("%d", &d.age);
@@ -249,7 +273,7 @@ void update() {
             printf("Enter new gender (M/F): ");
             scanf(" %c", &d.gender);
 
-            fseek(fp, -sizeof(d), SEEK_CUR);
+            fseek(fp,-sizeof(d), SEEK_CUR);
             fwrite(&d, sizeof(d), 1, fp);
 
             printf("Record updated successfully\n");
@@ -265,7 +289,21 @@ void update() {
 }
 
 void display() {
-    FILE *fp = fopen("user.data", "rb");
+    FILE *fp = fopen("data", "rb");
+    if (fp == NULL) {
+        printf("Error: Cannot open the file\n");
+        return;
+    }
+    while (fread(&d, sizeof(d), 1, fp)) {	
+        printf("Name: %s\nFather's Name: %s\nAddress: %s\nLandmark: %s\nDistrict: %s\nState: %s\nPincode: %d\nPhone: %ld\nDOB: %s\nREGISTRATION DATE:  %supdation date: %s\nAge: %d\nGender: %c\n :YOUR AADHAR NUMBER: \n\t\t   %lld\n\n", 
+            d.name, d.f_name, d.add, d.lm, d.dis, d.st, d.pin, d.phn, d.dob,d.cd,d.ud, d.age, d.gender, d.aadhar_no);
+           
+    }
+
+    fclose(fp);
+}
+void print_card() {
+    FILE *fp = fopen("data", "rb");
     if (fp == NULL) {
         printf("Error: Cannot open the file\n");
         return;
@@ -276,9 +314,9 @@ void display() {
    	 printf("\n------------- GOVERNMENT OF INDIA -------------\n");
     printf("\n------- UNIQUE IDENTIFICATION AUTHORITY OF INDIA -------\n\n");
     	
-        printf("Name: %s\nFather's Name: %s\nAddress: %s\nLandmark: %s\nDistrict: %s\nState: %s\nPincode: %d\nPhone: %ld\nDOB: %s\nAge: %d\nGender: %c\n  \t\t:YOUR AADHAR NUMBER: \n\t\t   %lld\n\n", 
-            d.name, d.f_name, d.add, d.lm, d.dis, d.st, d.pin, d.phn, d.dob, d.age, d.gender, d.aadhar_no);
-        printf("\n----------------THANKYOU FOR VISITING---------------\n");
+        printf("Name: %s\nFather's Name: %s\nAddress: %s\nLandmark: %s\nDistrict: %s\nState: %s\nPincode: %d\nPhone: %ld\nDOB: %s\nREGISTRATION DATE:  %s\nAge: %d\nGender: %c\n  \t\t:YOUR AADHAR NUMBER: \n\t\t   %lld\n\n", 
+            d.name, d.f_name, d.add, d.lm, d.dis, d.st, d.pin, d.phn, d.dob,d.cd, d.age, d.gender, d.aadhar_no);
+        printf("\n----------------MERA ADHAR MERI PEHCHAN---------------\n");
 		printf("---------------------------------------------------------------------------------\n");    
     }
 
@@ -286,7 +324,7 @@ void display() {
 }
 
 void remove_record() {
-    FILE *fp = fopen("user.data", "rb");
+    FILE *fp = fopen("data", "rb");
     FILE *temp_fp = fopen("temp.data", "wb");
 
     if (fp == NULL || temp_fp == NULL) {
@@ -316,12 +354,12 @@ void remove_record() {
     fclose(fp);
     fclose(temp_fp);
 
-    remove("user.data");
-    rename("temp.data", "user.data");
+    remove("data");
+    rename("temp.data", "data");
 }
 
 void count_records() {
-    FILE *fp = fopen("user.data", "rb");
+    FILE *fp = fopen("data", "rb");
     if (fp == NULL) {
         printf("Error: Cannot open the file\n");
         return;
@@ -338,22 +376,25 @@ void count_records() {
 }
 
 void list_all_users() {
-    FILE *fp = fopen("user.data", "rb");
+    FILE *fp = fopen("data", "rb");
     if (fp == NULL) {
         printf("Error: Cannot open the file\n");
         return;
     }
 
-    printf("\n------- List of Users -------\n");
+    printf("\n\t\t\t----------------LIST OF USERS------------------\n");
+    printf("-----------------------------------------------------------------------------------------------------\n");
+     printf("NAME\t\t\t\tAADHAR NUMBER\t\t\tREGISTRATION DATE\n");
+	printf("------------------------------------------------------------------------------------------------------\n");
     while (fread(&d, sizeof(d), 1, fp)) {
-        printf("Name: %s, Aadhar No: %lld\n", d.name, d.aadhar_no);
+        printf("%s\t\t\t\t%lld\t\t\t\t%s\n---------------------------------------------------------------------------------------------------------", d.name, d.aadhar_no,d.cd);
     }
 
     fclose(fp);
 }
 
 void search_record() {
-    FILE *fp = fopen("user.data", "rb");
+    FILE *fp = fopen("data", "rb");
     if (fp == NULL) {
         printf("Error: Cannot open the file\n");
         return;
@@ -368,8 +409,7 @@ void search_record() {
     while (fread(&d, sizeof(d), 1, fp)) {
         if (d.aadhar_no == aadhar_no) {
             printf("Record Found:\n");
-            printf("Name: %s\nFather's Name: %s\nAddress: %s\nLandmark: %s\nDistrict: %s\nState: %s\nPincode: %d\nPhone: %ld\nDOB: %s\nAge: %d\nGender: %c\nAadhar No: %lld\n\n", 
-                d.name, d.f_name, d.add, d.lm, d.dis, d.st, d.pin, d.phn, d.dob, d.age, d.gender, d.aadhar_no);
+            
             found = 1;
             break;
         }
